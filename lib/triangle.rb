14a2a0cffd0e44e4467e50703e8f89be0390b37a
Sara Garza <sjgarza22@gmail.com>
2020-01-30 15:21:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,12 @@
 class Triangle
+
+  attr_accessor :side1, :side2, :side3
+
+  def initilize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
   # write code here
   class TriangleError < StandardError
     # triangle error code
